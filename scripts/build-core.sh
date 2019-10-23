57b39e0b9c55f1a6808c16b54a080a63ad9206bf
Tim Williamson <timwilliamson1337@gmail.com>
2019-10-23 14:54:53 +0000

    add ready promise to build that resolves with the module once it is fully initialized

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -1,5 +1,11 @@
 if which emcc ; then
-    emcc lib/c++/src/tree-builder.cpp -o lib/js/build/build.js
+    emcc --bind lib/c++/src/tree-builder.cpp -s WASM=1 -o lib/js/build/build.js
+    
+    echo 'built'
+    
+    sed -i '20 i Module.ready = new Promise(res => {Module.onRuntimeInitialized = () => {res(Module)}});' lib/js/build/build.js
+    
+    echo 'added ready function'
 else
     echo "emcc not found. If you have installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
 fi
