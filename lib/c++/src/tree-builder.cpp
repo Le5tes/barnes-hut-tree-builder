b0aa886c1e9d7297550cba7a228f8e374635f0d7
Tim Williamson <timwilliamson1337@gmail.com>
2019-10-23 15:13:37 +0000

    add wrapper class with async create method that interfaces the wasm module

--- lib/c++/src/tree-builder.cpp
+++ lib/c++/src/tree-builder.cpp
@@ -1,6 +1,16 @@
 #include "tree-builder.h"
+#include <emscripten/bind.h>
+using namespace emscripten;
 
 TreeBuilder getTreeBuilder() {
     TreeBuilder treeBuilder;
     return treeBuilder;
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    function("getTreeBuilder", &getTreeBuilder);
+
+    class_<TreeBuilder>("TreeBuilder")
+    .constructor<>()
+    .function("build", &TreeBuilder::build, allow_raw_pointers());
 }
