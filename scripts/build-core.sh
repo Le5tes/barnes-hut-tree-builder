88c30c1c8cd97df9e42c5c9c0061e544695f943d
Tim Williamson <timwilliamson1337@gmail.com>
2019-10-28 13:35:53 +0000

    update build script

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -1,11 +1,7 @@
 if which emcc ; then
-    emcc --bind lib/c++/src/tree-builder.cpp -s WASM=1 -o lib/js/build/build.js
-    
-    echo 'built'
-    
+    emcc --bind lib/c++/src/tree-builder.binder.cpp -s WASM=1 -o lib/js/build/build.js
     sed -i '20 i Module.ready = new Promise(res => {Module.onRuntimeInitialized = () => {res(Module)}});' lib/js/build/build.js
-    
-    echo 'added ready function'
+    echo 'built'
 else
     echo "emcc not found. If you have installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
 fi
