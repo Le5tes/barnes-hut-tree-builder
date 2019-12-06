ff22cbc277feaf9dc0a98b92a6ed2e4575e7b9fc
Tim Williamson <timwilliamson1337@gmail.com>
2019-12-06 15:13:06 +0000

    update buildspec.yml - get emsdk

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -7,6 +7,7 @@ if which emcc ; then
     sed -i '20 i Module.ready = new Promise(res => {Module.onRuntimeInitialized = () => {res(Module)}});' lib/js/build/build.js
     echo 'built'
 else
-    echo "emcc not found. If you have installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
+    echo "emcc not found. If you hqave installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
+    exit 1;
 fi
     
