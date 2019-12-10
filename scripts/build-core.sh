84100cfb1e7387add681d2ed3ca9d644b793349b
Tim Williamson <timwilliamson1337@gmail.com>
2019-12-10 14:00:59 +0000

    chore() update 'build' directory name to 'core'

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -1,10 +1,10 @@
 if which emcc ; then
-    if [ ! -d "lib/js/build" ]; then
-      mkdir lib/js/build
+    if [ ! -d "lib/js/core" ]; then
+      mkdir lib/js/core
     fi
 
     emcc --bind lib/c++/src/tree-builder.binder.cpp -s WASM=1 -o lib/js/build/build.js
-    sed -i '20 i Module.ready = new Promise(res => {Module.onRuntimeInitialized = () => {res(Module)}});' lib/js/build/build.js
+    sed -i '20 i Module.ready = new Promise(res => {Module.onRuntimeInitialized = () => {res(Module)}});' lib/js/core/build.js
     echo 'built'
 else
     echo "emcc not found. If you hqave installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
