f03cf28fcc223664ac128dfb40e21c4ac784aaf8
Le5tes <timwilliamson1337@gmail.com>
2020-01-09 17:00:43 +0000

    feat(build-core.sh) allow memory growth to remove limit on body array size

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -3,11 +3,11 @@ if which emcc ; then
       mkdir lib/js/core
     fi
 
-    emcc --bind lib/c++/src/tree-builder.binder.cpp -s WASM=1 -o lib/js/core/build.js
+    emcc --bind lib/c++/src/tree-builder.binder.cpp -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -o lib/js/core/build.js
     sed -i '20 i Module.ready = new Promise(res => {Module.onRuntimeInitialized = () => {res(Module)}});' lib/js/core/build.js
     echo 'built'
 else
-    echo "emcc not found. If you hqave installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
+    echo "emcc not found. If you have installed emscripten, navigate to you emskd folder and run '$source ./emsdk_env.sh', else see https://emscripten.org/docs/getting_started/downloads.html#sdk-download-and-install"
     exit 1;
 fi
     
